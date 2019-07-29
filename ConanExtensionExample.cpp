9844df7549be3eac7feb4597d510b7c5d4f2285f
Jimmy Dee <jgvdthree@gmail.com>
2019-07-29 12:40:26 -0700

    Actually use Poco in the program

--- ConanExtensionExample.cpp
+++ ConanExtensionExample.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+#include <Poco/Environment.h>
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Poco::Environment os;
+    std::cout << "Hello " << os.osName() << "!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
